13b3f143e3964929e9ab0bce636dd3d2d9b3cecd
fullere <fullere@hartwick.edu>
2019-11-13 16:27:03 -0500

    Made class Person and tested it

--- Driver.py
+++ Driver.py
@@ -1,3 +1,11 @@
 # Elizabeth Fuller
 # 11-13-19
 # Test file for person class
+from Person import Person
+
+person1 = Person("Elizabeth", "Fuller", 19, "Liz")
+
+print(person1.description())
+person1.birthday()
+person1.change_nickname("Eliza")
+print(person1.description())
--- Person.py
+++ Person.py
@@ -1,3 +1,21 @@
 # Elizabeth Fuller
 # 11/13/19
 # Person Class file
+
+
+class Person:
+    def __init__(self, first, last, age, nickname):
+        self.first = first
+        self.last = last
+        self.age = age
+        self.nickname = nickname
+
+    def description(self):
+        return self.first + " " + self.last + " is " + str(self.age) + " and has the nickname " + self.nickname + "."
+
+    def birthday(self):
+        self.age = self.age + 1
+
+    def change_nickname(self, new_nickname):
+        self.nickname = new_nickname
+
